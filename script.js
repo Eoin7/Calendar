3a8afcde5277554768319524227b6b48b5320d90
Eoin Foley <eoinafoley@gmail.com>
2026-06-08 00:36:06 +0100

    Initial Calendar

---
+++ script.js
@@ -0,0 +1,33 @@
+const daysContainer = document.querySelector(".days");
+
+
+for (let day = 1; day <= 30; day++) {
+    const dayDiv = document.createElement("div");
+
+    dayDiv.classList.add("day");
+    dayDiv.textContent = day;
+
+    daysContainer.appendChild(dayDiv);
+}
+
+// const day7 = document.querySelector(".days").children[6];
+// // day7.style.backgroundColor = "#2144ab";
+
+
+for (let i = 0; i < 5; i++) {
+    const emptyDiv = document.createElement("div");
+    emptyDiv.classList.add("day", "empty");
+    daysContainer.appendChild(emptyDiv);
+}
+
+const namesContainer = document.querySelector(".names");
+const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+
+days.forEach(name => {
+    const nameDiv = document.createElement("div");
+
+    nameDiv.classList.add("name");
+    nameDiv.textContent = name;
+
+    namesContainer.appendChild(nameDiv);
+})
